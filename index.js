56e99d0056ba2e4fd04ec8c551f093bca646a411
Simona Šusterová <simsusterova@gmail.com>
2025-04-03 13:53:35 +0200

    Hod minci

---
+++ index.js
@@ -0,0 +1,15 @@
+const vysledek = document.querySelector('.vysledek');
+const padlOrel = Math.random() < 0.5;
+const mince = document.querySelector('.mince');
+
+
+
+if (padlOrel) {
+  vysledek.textContent = 'Padl orel';
+  mince.classList.add('mince--orel');
+} 
+
+else {
+  vysledek.textContent = 'Padla panna';
+  mince.classList.add('mince--panna');
+}
